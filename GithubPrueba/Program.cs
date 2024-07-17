e29d1ff80a41f051391bdaf0cce9f5d18f16053b
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-07-17 11:36:22 -0600

    Funciones de suma y resta

--- GithubPrueba/Program.cs
+++ GithubPrueba/Program.cs
@@ -1,6 +1,9 @@
-﻿int opcion;
+﻿using System.ComponentModel;
+
+int opcion;
 do
 {
+    Console.Clear();
     Console.WriteLine("Menu de Operaciones");
     Console.WriteLine("1. SUMA");
     Console.WriteLine("2. RESTA");
@@ -15,12 +18,68 @@ do
             {
                 Console.Clear();
                 Console.WriteLine("SUMA");
+                double primerNumero = double.MaxValue;
+                double segundoNumero= double.MaxValue;
+                bool continuarSuma=true;
+                while (continuarSuma)
+                {
+                    try
+                    {
+                        if (primerNumero == double.MaxValue)
+                        {
+                            Console.WriteLine("Ingrese el primer número.");
+                            primerNumero= Convert.ToDouble(Console.ReadLine());
+                        }
+                        if (segundoNumero == double.MaxValue)
+                        {
+                            Console.WriteLine("Ingrese el segundo número.");
+                            segundoNumero = Convert.ToDouble(Console.ReadLine());
+                        }
+                        double suma = segundoNumero + primerNumero;
+                        Console.WriteLine($"{primerNumero} + {segundoNumero} = {suma}.");
+                        continuarSuma= false; 
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Debe ingresar un número." + ex.Message);
+                    }
+                }
+                Console.ReadKey();
                 break;
             }
         case 2:
             {
                 Console.Clear ();
                 Console.WriteLine("RESTA");
+                double primerNumero= double.MaxValue;
+                double segundoNumero= double.MaxValue;
+                bool continuarResta=true;
+                while (continuarResta)
+                {
+                    try
+                    {
+                        if (primerNumero == double.MaxValue)
+                        {
+                            Console.WriteLine("Ingrese el primer número.");
+                            primerNumero=Convert.ToDouble(Console.ReadLine());
+                        }
+                        if (segundoNumero == double.MaxValue)
+                        {
+                            Console.WriteLine("Ingrese el segundo número.");
+                            segundoNumero=Convert.ToDouble (Console.ReadLine());
+                        }
+                        double resta= primerNumero-segundoNumero;
+                        Console.WriteLine($"{primerNumero} - {segundoNumero} = {resta}");
+                        continuarResta = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Debe ingresar un número." + ex.Message);
+                    }
+                }
+                Console.ReadKey();
                 break;
             }
         case 3:
