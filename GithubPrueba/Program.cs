2ddc2d60da493959155edf957ad12c92a8d8925a
Josué Soch <Shonsoch@gmail.com>
2024-07-17 09:21:27 -0600

    se puso un menu

--- GithubPrueba/Program.cs
+++ GithubPrueba/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿do
+{
+    Console.WriteLine("Menu de Operaciones");
+    Console.WriteLine("1. SUMA");
+    Console.WriteLine("2. RESTA");
+    Console.WriteLine("3. MULTIPLICACION");
+    Console.WriteLine("4. DIVISION");
+    Console.WriteLine("5. salir");
+    Console.WriteLine("Eliga una de las siguientes opciones");
+
+}while(true);
