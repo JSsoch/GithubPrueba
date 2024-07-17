55447646357cacb047cefd5f425d768182390568
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-07-17 09:38:23 -0600

    Menu estructura

--- GithubPrueba/Program.cs
+++ GithubPrueba/Program.cs
@@ -1,4 +1,5 @@
-﻿do
+﻿int opcion;
+do
 {
     Console.WriteLine("Menu de Operaciones");
     Console.WriteLine("1. SUMA");
@@ -7,5 +8,38 @@
     Console.WriteLine("4. DIVISION");
     Console.WriteLine("5. salir");
     Console.WriteLine("Eliga una de las siguientes opciones");
-
-}while(true);
+    opcion = Convert.ToInt32(Console.ReadLine());
+    switch (opcion)
+    {
+        case 1:
+            {
+                Console.Clear();
+                Console.WriteLine("SUMA");
+                break;
+            }
+        case 2:
+            {
+                Console.Clear ();
+                Console.WriteLine("RESTA");
+                break;
+            }
+        case 3:
+            {
+                Console.Clear();
+                Console.WriteLine("MULTIPLICACION");
+                break;
+            }
+        case 4:
+            {
+                Console.Clear();
+                Console.WriteLine("DIVISION");
+                break;
+            }
+        case 5:
+            {
+                Console.Clear();
+                opcion = 0;
+                break;
+            }
+    }
+} while (opcion != 0);
